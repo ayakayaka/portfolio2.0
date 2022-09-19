2618e67b4d2baa2f7bf04cfa183cfdbc5e903a10
Ayaka Inaba <ayakayakaaaa@gmail.com>
2022-09-19 16:20:31 +0900

    probably done, unable to add js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,26 @@
 // This is where it all goes :)
-new TypeIt("#mainTitle", {
-  strings: "Ayaka Inaba's Portfolio",
-  speed: 50,
-  waitUntilVisible: true,
-}).go();
+
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+
+var typed = new Typed('.element', {
+  strings: ["First sentence.", "Second sentence."],
+  typeSpeed: 30
+});
+
+
+// export default class extends Controller {
+//   connect() {
+//     new Typed(this.element, {
+//       strings: ["Ayaka Inaba's Portfolio"],
+//       typeSpeed: 50,
+//       loop: true
+//     })
+//   }
+// }
+// new TypeIt("#mainTitle", {
+//   strings: "Ayaka Inaba's Portfolio",
+//   speed: 50,
+//   waitUntilVisible: true,
+// }).go();
